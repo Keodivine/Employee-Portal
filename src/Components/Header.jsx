8919ce556b64fa516f15f9ac99ebb0bff91fe8c5
Keodivine <tsiekeorapetse45@gmail.com>
2024-07-24 15:38:13 +0200

    changed heading

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = ({ onSearch }) => {
   return (
     <header className="App-header">
-      <h1>Employee Managements Portal</h1>
+      <h1>Employee Management Portal</h1>
       <input
         type="text"
         placeholder="Search by name, surname, ID, or department"
